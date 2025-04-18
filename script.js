b24d1bc2b8b516725335952b0cb5871f70d2c39f
tanishhh7 <shettytanish.ramesh2023@vitstudent.ac.in>
2025-04-18 17:31:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".india-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+document.addEventListener('click', function(event) {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".india-icon");
+    const menuContainer = document.querySelector(".india-menu");
+
+
+    if (!menuContainer.contains(event.target)) {
+        menu.classList.remove("open");
+        icon.classList.remove("open");
+    }
+});
+
+const menuLinks = document.querySelectorAll(".menu-links a");
+menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        const menu = document.querySelector(".menu-links");
+        const icon = document.querySelector(".india-icon");
+        menu.classList.remove("open");
+        icon.classList.remove("open");
+    });
+});
